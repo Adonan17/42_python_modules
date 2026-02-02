ac1f2d98e2077c04a73bd2acad58f37a20fa9983
Adonan17 <adnaneouassari@gmail.com>
2026-02-02 16:32:26 +0100

    creating both files for exercise 5 and finishing ft_count_harvest_iterative function

---
+++ module00/ex5/ft_count_harvest_iterative.py
@@ -0,0 +1,10 @@
+def ft_count_harvest_iterative():
+    days = int(input("Days until harvest: "))
+    i = 1
+    while i <= days:
+        print(f"Day {i}")
+        i += 1
+
+
+if __name__ == "__main__":
+    ft_count_harvest_iterative()
