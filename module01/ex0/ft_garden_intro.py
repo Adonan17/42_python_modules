106ef283a2b7327488442a86b10865ced4c9d597
Adonan17 <adnaneouassari@gmail.com>
2026-02-04 13:09:08 +0100

    solving ex0 mod01

---
+++ module01/ex0/ft_garden_intro.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def ft_garden_intro():
+    plant = "Rose"
+    height = "25cm"
+    age = 30
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {plant}\nHeight: {height}\nAge: {age} days")
+    print("\n=== End of Program ===")
+
+
+if __name__ == "__main__":
+    ft_garden_intro()
