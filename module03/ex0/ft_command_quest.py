8865eaa64e7ec7ae6d2eb3c17c641ed6810d0ca4
Adonan17 <adnaneouassari@gmail.com>
2026-03-12 14:17:59 +0100

    solving ex0 mod03

---
+++ module03/ex0/ft_command_quest.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import sys
+
+print("=== Command Quest ===")
+print(f"Program name: {sys.argv[0]}")
+
+if len(sys.argv) == 1:
+    print("No arguments provided!")
+    print(f"Total arguments: {len(sys.argv)}")
+else:
+    print(f"Arguments received: {len(sys.argv) - 1}")
+    i = 1
+    while i < len(sys.argv):
+        print(f"Argument {i}: {sys.argv[i]}")
+        i += 1
+    print(f"Total arguments: {len(sys.argv)}")
